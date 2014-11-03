a247c8e92937626bcfe80ce13b5358936f202c4d
chuc3 <chuc3@rpi.edu>
2014-11-03 01:09:05 -0500

    stuff'

---
+++ Assets/Scripts/mapSpawnScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapSpawnScript : MonoBehaviour {
+
+	public Transform tile;
+	public TextAsset map;
+
+
+	void Awake() {
+		// subdivide map by newline chars
+		string [] lines = map.text.Split ('\n'); int i, j,d; i=j=d=0;
+
+		foreach (string l in lines)
+		{
+			foreach (char c in l)
+			{
+				if(c=='.' && d==0)
+				{
+					d=3;
+				}
+				else if(c == '.' && d>0)
+				{
+					d--;
+				}
+				else
+				{
+					Vector3 v = new Vector3(i,j,0f);
+					GameObject g = new GameObject();
+					g = Instantiate(tile,v, Quaternion.identity) as GameObject;
+					if(c=='.')
+						tile.renderer.material.color = Color.blue;
+					else
+						tile.renderer.material.color = Color.red;
+					i++;
+				}
+
+			}
+			i=0;j++;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
